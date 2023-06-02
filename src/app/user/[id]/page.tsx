2fc7aa0cdf10e5c6a51bdec0622886d24dfe2953
alexbarker234 <alex.barker234@gmail.com>
2023-06-02 08:43:35 +0800

    switched to react suspense

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,12 +1,15 @@
 import Base from "@/components/base";
 import UrlForm from "@/components/urlform";
 import Wishlist from "./wishlist";
+import { Suspense } from "react";
 
 export default async function Home({ params }: any) {
     return (
         <Base title="index">
             <h1>User</h1>
-            <Wishlist id={params.id} />
+            <Suspense fallback={"loading test..."}>
+                <Wishlist id={params.id} />
+            </Suspense>
         </Base>
     );
 }
--- src/app/user/[id]/wishlist.tsx
+++ src/app/user/[id]/wishlist.tsx
@@ -1,26 +1,17 @@
 "use client";
-import React, { useState, useEffect } from "react";
 import { WishlistItem } from "@/types";
 
 interface WishlistProp {
     id: string;
 }
 
-const Wishlist = ({ id }: WishlistProp) => {
-    const [data, setData] = useState<WishlistItem[]>();
-
+const Wishlist = async ({ id }: WishlistProp) => {
     // keep up to date with https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#use-in-client-components
-    // fetch cant be done in use safely yet but it will be useful :) 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch(`/api/get-wishlist?id=${id}`);
-            const data: WishlistItem[] = await res.json();
+    // fetch cant be done in use safely yet but it will be useful :)
+    const res = await fetch(`/api/get-wishlist?id=${id}`);
 
-            setData(data);
-        };
-
-        fetchData();
-    }, [id]);
+    const data = await res.json()
+    console.log(data)
 
     return <p>{JSON.stringify(data)}</p>;
 };
