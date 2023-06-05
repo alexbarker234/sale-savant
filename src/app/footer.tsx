c9646b8b201c5dbec55608726a2059cb7293ad65
alexbarker234 <alex.barker234@gmail.com>
2023-06-05 16:02:42 +0800

    fixed typo

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="footer">
             <div>By <a href="https://github.com/alexbarker234">Alex Barker</a></div>
             <div className="links">
-                <a href="https://github.com/alexbarker234/sale-savant">Github</a>
+                <a href="https://github.com/alexbarker234/sale-savant">GitHub</a>
             </div>
         </div>
     );
