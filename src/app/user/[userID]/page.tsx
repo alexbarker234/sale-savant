6410b3b7c4425d3db12a5c7e0272e8212db4faf2
alexbarker234 <alex.barker234@gmail.com>
2023-11-10 15:09:12 +0800

    Use Steam.getUser directly instead of api route

--- src/app/user/[userID]/page.tsx
+++ src/app/user/[userID]/page.tsx
@@ -2,13 +2,11 @@ import UserInfo from "./userInfo";
 import Wishlist from "./wishlist";
 import { use } from "react";
 import { redirect } from 'next/navigation';
+import { Steam } from "@/lib/steam";
 
 export default function Home({ params }: any) {
-    const data= use(use(fetch(`${process.env.URL}/api/get-user-details?id=${params.userID}`, { next: { revalidate: 600 } })).json());
-
-    if (data.error) {
-        redirect('/')
-    }
+    const data = use(Steam.getUser(params.userID));
+    if (!data) redirect('/')
 
     return (
         <>
