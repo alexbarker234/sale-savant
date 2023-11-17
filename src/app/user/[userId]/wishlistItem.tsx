08b767e5e452e60ca3e71e2b2d095d376e0f8d4f
alexbarker234 <alex.barker234@gmail.com>
2023-11-17 18:59:43 +0800

    fixed discount tag hover

--- src/app/user/[userId]/wishlistItem.tsx
+++ src/app/user/[userId]/wishlistItem.tsx
@@ -5,7 +5,7 @@ import styles from "./wishlistItem.module.scss";
 
 const WishlistItem = ({ index, item }: { index: number; item: WishlistItem }) => {
     return (
-        <div className={styles["wishlist-item"]} style={item.show ? {} : {display: "none"}}>
+        <div className={styles["wishlist-item"]} style={item.show ? {} : { display: "none" }}>
             <Image width={296} height={136} src={item.imageURL} alt={`Banner art for ${item.gameName}`} />
             <div className={styles["content"]}>
                 <div className={styles["details"]}>
@@ -31,22 +31,23 @@ const WishlistItem = ({ index, item }: { index: number; item: WishlistItem }) =>
 
 function Price({ price, type }: { price: Deal; type: string }) {
     return (
-        <a href={`https://www.cheapshark.com/redirect?dealID=${price.dealID}`}>
-            <div className={`${styles["price"]} ${styles[type + "-price"]} ${price.discountPercent != 0 && styles["discounted"]}`}>
-                {price.discountPercent != 0 ? <div className={styles["discount"]}>-{price.discountPercent}%</div> : ""}
-                <div className={styles["price-details"]}>
-                    <svg width="50" height="50" fill="#ffffff" viewBox="0 0 150 150">
-                        <use href={`#${type}-logo`} />
-                    </svg>
+        <a
+            href={`https://www.cheapshark.com/redirect?dealID=${price.dealID}`}
+            className={`${styles["price"]} ${styles[type + "-price"]} ${price.discountPercent != 0 && styles["discounted"]}`}
+        >
+            {price.discountPercent != 0 ? <div className={styles["discount"]}>-{price.discountPercent}%</div> : ""}
+            <div className={styles["price-details"]}>
+                <svg width="50" height="50" fill="#ffffff" viewBox="0 0 150 150">
+                    <use href={`#${type}-logo`} />
+                </svg>
 
-                    <div className={styles["price-tag"]}>
-                        {price.originalPrice && price.originalPrice != price.currentPrice ? (
-                            <div className={styles["original-price"]}>USD${price.originalPrice.toFixed(2)}</div>
-                        ) : (
-                            ""
-                        )}
-                        <div className={styles["final-price"]}>USD${price.currentPrice.toFixed(2)}</div>
-                    </div>
+                <div className={styles["price-tag"]}>
+                    {price.originalPrice && price.originalPrice != price.currentPrice ? (
+                        <div className={styles["original-price"]}>USD${price.originalPrice.toFixed(2)}</div>
+                    ) : (
+                        ""
+                    )}
+                    <div className={styles["final-price"]}>USD${price.currentPrice.toFixed(2)}</div>
                 </div>
             </div>
         </a>
