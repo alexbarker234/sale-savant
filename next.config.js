31934d1830b06ce517e6c1a612d90b57d0c6b55e
alexbarker234 <alex.barker234@gmail.com>
2023-12-12 00:06:26 +0800

    added alternative steam cdn

--- next.config.js
+++ next.config.js
@@ -19,6 +19,12 @@ module.exports = {
                 port: "",
                 pathname: "/**",
             },
+            {
+                protocol: "https",
+                hostname: "cdn.akamai.steamstatic.com",
+                port: "",
+                pathname: "/**",
+            },
         ],
     },
 };
