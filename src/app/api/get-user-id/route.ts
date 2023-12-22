fdf5233a527c518c2371e869ece3224c61395dfc
alexbarker234 <alex.barker234@gmail.com>
2023-12-22 10:02:09 +0800

    workflows and prettier

--- src/app/api/get-user-id/route.ts
+++ src/app/api/get-user-id/route.ts
@@ -4,18 +4,17 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
     const url = searchParams.get("url");
-    if (!url)
-    {
+    if (!url) {
         const response = { error: "No url provided" };
         return NextResponse.json(response, { status: 400 });
     }
 
-    let steamUserId = await Steam.resolveUserFromURL(url)
+    let steamUserId = await Steam.resolveUserFromURL(url);
     if (!steamUserId) {
         const response = { error: "No user found" };
         return NextResponse.json(response, { status: 404 });
     }
 
-    const response = {id: steamUserId}
+    const response = { id: steamUserId };
     return NextResponse.json(response);
 }
